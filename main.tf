cdb946674a55b84f509ce5c48c63ce5c74473e53
unknown <jayant.atul@bharuwasolution.com>
2023-01-31 09:41:45 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-00874d747dde814fa"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "example-tf"
+  }
+}
